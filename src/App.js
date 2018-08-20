f01914c13f8ef2c3698f76c078e7d01bf885e096
xiaohesong <didmehh@163.com>
2018-08-20 14:33:13 +0800

    remove

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Input from './components/Input'
+import Input from './components/inputs/Input'
 
 const hello = () => (
   <div className='app'>
