b35118a72fe2420fc81cdba71bd21ad0f508a1c0
xiaohesong <didmehh@163.com>
2018-08-20 14:30:51 +0800

    clean dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+//https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const handleCss = new MiniCssExtractPlugin({
+  filename: '[name].[contenthash:8].css',
+  chunkFilename: '[name].[contenthash:8].chunk.css',
+})
+const needClean = new CleanWebpackPlugin(['dist'])
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./public/index.html",
   filename: "./index.html"
 });
 
+
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    chunkFilename: '[name].[chunkhash:8].chunk.js',
+    filename: '[name].[chunkhash:8].js'
   },
   module: {
     rules: [{
@@ -27,7 +38,7 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      use: ["style-loader", "css-loader"]
+      use: [MiniCssExtractPlugin.loader, "css-loader"]
     },
     {
       test: /\.(png|svg|jpg|gif|jpeg)$/,
@@ -35,5 +46,5 @@ module.exports = {
     }
   ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlPlugin, needClean, handleCss]
 };
