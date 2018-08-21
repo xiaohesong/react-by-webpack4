221a4b8ef28bd2d63864f2a902a8e64a9ce33730
xiaohesong <didmehh@163.com>
2018-08-21 15:34:48 +0800

    use postcss

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 //https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const autoprefixer = require('autoprefixer');
+
 // Plugins
 const handleCss = new MiniCssExtractPlugin({
   filename: '[name].[contenthash:8].css',
@@ -25,6 +27,25 @@ const lessLoader = {
       'link-color': '#1DA57A',
     },
     javascriptEnabled: true
+  }
+}
+
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    ident: 'postcss',
+    plugins: () => [
+      require('postcss-flexbugs-fixes'),
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9', // React doesn't support IE8 anyway
+        ],
+        flexbox: 'no-2009',
+      }),
+    ], //postcss plugins end
   }
 }
 
@@ -56,7 +77,7 @@ module.exports = {
     },
     {
       test: /\.(less|css)$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader', lessLoader] // end less use
+      use: [MiniCssExtractPlugin.loader, 'css-loader', postcssLoader, lessLoader] // end less use
     },
     {
       test: /\.(png|svg|jpg|gif|jpeg)$/,
