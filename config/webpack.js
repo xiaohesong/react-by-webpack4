aceffc787491b0644fcf655de44ad635b790900b
xiaohesong <didmehh@163.com>
2018-09-07 20:12:37 +0800

    webpack config to another file

---
+++ config/webpack.js
@@ -0,0 +1,130 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+//https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const autoprefixer = require('autoprefixer');
+
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+// Plugins
+const handleCss = new MiniCssExtractPlugin({
+  filename: 'static/css/[name].[contenthash:8].css',
+  chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+})
+const needClean = new CleanWebpackPlugin(['dist'])
+const bundleView = new BundleAnalyzerPlugin()
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+  filename: "./index.html",
+  minify: {
+    removeComments: true,
+    collapseWhitespace: true,
+    removeRedundantAttributes: true,
+    useShortDoctype: true,
+    removeEmptyAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    keepClosingSlash: true,
+    minifyJS: true,
+    minifyCSS: true,
+    minifyURLs: true,
+  },
+});
+
+const lessLoader = {
+  loader: 'less-loader', // compiles Less to CSS
+  options: {
+    modifyVars: {
+      'primary-color': '#1DA57A',
+      'link-color': '#1DA57A',
+    },
+    javascriptEnabled: true
+  }
+}
+
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    importLoaders: 1,
+    minimize: true,
+  }
+}
+
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    ident: 'postcss',
+    plugins: () => [
+      require('postcss-flexbugs-fixes'),
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9', // React doesn't support IE8 anyway
+        ],
+        flexbox: 'no-2009',
+      }),
+    ], //postcss plugins end
+  }
+}
+
+const splitChunks = {
+  chunks: 'all',
+  cacheGroups: {
+    commons: {
+      name: 'commons',
+      minChunks: 2
+    },
+    vendor: {
+      test: /[\\/]node_modules[\\/]/,
+      name: 'vendor',
+    },
+  }
+}
+
+const preloadPlugin = new PreloadWebpackPlugin()
+const OptimizeCSSAssets = new OptimizeCSSAssetsPlugin({ cssProcessorOptions: { safe: true } })
+const uglifyConfig = new UglifyJsPlugin({
+  uglifyOptions: {
+    parse: {
+      ecma: 8,
+    },
+    compress: {
+      ecma: 5,
+      warnings: false,
+      comparisons: false,
+    },
+    mangle: {
+      safari10: true,
+    },
+    output: {
+      ecma: 5,
+      comments: false,
+      ascii_only: true,
+    },
+  },
+  parallel: true,
+  cache: true,
+  sourceMap: false,
+})
+
+
+module.exports = {
+  handleCss,
+  needClean,
+  htmlPlugin,
+  lessLoader,
+  cssLoader,
+  postcssLoader,
+  preloadPlugin,
+  OptimizeCSSAssets,
+  uglifyConfig,
+  MiniCssExtractPlugin,
+  splitChunks,
+  bundleView,
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,122 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const configs = require('./config/webpack.js')
 
-//https://webpack.js.org/plugins/mini-css-extract-plugin/#minimizing-for-production
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
-const autoprefixer = require('autoprefixer');
-
+const IgnorePlugin = new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
 const getClientEnvironment = require('./config/env.js')
-const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-// Plugins
-const handleCss = new MiniCssExtractPlugin({
-  filename: 'static/css/[name].[contenthash:8].css',
-  chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-})
-const needClean = new CleanWebpackPlugin(['dist'])
-const bundleView = new BundleAnalyzerPlugin()
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./public/index.html",
-  filename: "./index.html",
-  minify: {
-    removeComments: true,
-    collapseWhitespace: true,
-    removeRedundantAttributes: true,
-    useShortDoctype: true,
-    removeEmptyAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    keepClosingSlash: true,
-    minifyJS: true,
-    minifyCSS: true,
-    minifyURLs: true,
-  },
-});
-
-const lessLoader = {
-  loader: 'less-loader', // compiles Less to CSS
-  options: {
-    modifyVars: {
-      'primary-color': '#1DA57A',
-      'link-color': '#1DA57A',
-    },
-    javascriptEnabled: true
-  }
-}
-
-const cssLoader = {
-  loader: 'css-loader',
-  options: {
-    importLoaders: 1,
-    minimize: true,
-  }
-}
-
-const postcssLoader = {
-  loader: 'postcss-loader',
-  options: {
-    ident: 'postcss',
-    plugins: () => [
-      require('postcss-flexbugs-fixes'),
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9', // React doesn't support IE8 anyway
-        ],
-        flexbox: 'no-2009',
-      }),
-    ], //postcss plugins end
-  }
-}
-
-const splitChunks = {
-  chunks: 'all',
-  cacheGroups: {
-    commons: {
-      name: 'commons',
-      minChunks: 2
-    },
-    vendor: {
-      test: /[\\/]node_modules[\\/]/,
-      name: 'vendor',
-    },
-  }
-}
-
-const preloadPlugin = new PreloadWebpackPlugin()
-const OptimizeCSSAssets = new OptimizeCSSAssetsPlugin({ cssProcessorOptions: { safe: true } })
-const uglifyConfig = new UglifyJsPlugin({
-  uglifyOptions: {
-    parse: {
-      ecma: 8,
-    },
-    compress: {
-      ecma: 5,
-      warnings: false,
-      comparisons: false,
-    },
-    mangle: {
-      safari10: true,
-    },
-    output: {
-      ecma: 5,
-      comments: false,
-      ascii_only: true,
-    },
-  },
-  parallel: true,
-  cache: true,
-  sourceMap: false,
-})
-
-const IgnorePlugin =  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
 
 module.exports = (_env, args) => {
   const env = getClientEnvironment(args.mode);
@@ -143,8 +30,8 @@ module.exports = (_env, args) => {
       // runtimeChunk: true,
 
       minimizer: [
-        uglifyConfig,
-        OptimizeCSSAssets
+        configs.uglifyConfig,
+        configs.OptimizeCSSAssets
       ]
     },
     module: {
@@ -180,7 +67,7 @@ module.exports = (_env, args) => {
         },
         {
           test: /\.(less|css)$/,
-          use: [MiniCssExtractPlugin.loader, cssLoader, postcssLoader, lessLoader] // end less use
+          use: [configs.MiniCssExtractPlugin.loader, configs.cssLoader, configs.postcssLoader, configs.lessLoader] // end less use
         },
         {
           test: /\.(png|svg|jpg|gif|jpeg)$/,
@@ -189,7 +76,7 @@ module.exports = (_env, args) => {
       ]
     },
     // if need to show bundle package size, add bundleView to plugins
-    plugins: [htmlPlugin, needClean, handleCss, DefinePlugin, IgnorePlugin, preloadPlugin],
+    plugins: [configs.htmlPlugin, configs.needClean, configs.handleCss, DefinePlugin, IgnorePlugin, configs.preloadPlugin],
     
     resolve: {
       extensions: [".ts", ".tsx", ".js"]
