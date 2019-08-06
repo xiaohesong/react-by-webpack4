49868fdaf5ac7ef1faf2efce86e12be0f143ff40
风马牛不相及 <didmehh@163.com>
2019-08-06 18:40:45 +0800

    typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux'
 import configureStore from './configureStore'
-import App from './app'
+import App from './App'
 
 import * as serviceWorker from './serviceWorker';
 
